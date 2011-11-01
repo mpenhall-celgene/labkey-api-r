fb37bb53785fdb69887e9eeca6678874a59245e3
Kevin Krouse <kevink@labkey.com>
2011-11-01 23:39:23 +0000

    add flowStats dependency
    SVN r17830 |2011-11-01 23:39:23 +0000

--- Rpackages/install-flowWorkspace-dependencies.R
+++ Rpackages/install-flowWorkspace-dependencies.R
@@ -22,4 +22,5 @@ if (vers < "2.13") {
 source("../../tools/Rpackages/install-util.R")
 install.dependencies("flowWorkspace",
     c("rrcov", "feature", "robustbase", "pcaPP", "mvtnorm", "ks"),
-    c("RBGL", "graph", "XML", "flowCore", "flowViz", "Rgraphviz", "Biobase", "IDPmisc", "flowStats"))
+    c("RBGL", "graph", "XML", "flowCore", "flowViz", "Rgraphviz", "Biobase", "IDPmisc"))
+
